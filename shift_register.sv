8b341d8f7aa836030fc99e8a43e2456eef0bd79c
tagbrit <39653862+tagbrit@users.noreply.github.com>
2018-06-06 17:23:23 -0700

    Update shift_register.sv

--- shift_register.sv
+++ shift_register.sv
@@ -1,7 +1,7 @@
 module shift_register #(parameter N = 8) (
 	input logic clk,
 	input logic reset,
-	input logic load,
+	input logic load, //jjj
 	input logic s_in,
 	input logic [N-1:0] d,
 	output logic [N-1:0] q,
