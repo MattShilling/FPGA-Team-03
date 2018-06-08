3b9212f92af7f76c3641ac67b8ccac5f4c1bb139
Marissa Kwon <kwonma@oregonstate.edu>
2018-06-07 20:55:58 -0700

    Update s2p_shift_register.sv

--- s2p_shift_register.sv
+++ s2p_shift_register.sv
@@ -1,11 +1,18 @@
 // http://www.csit-sun.pub.ro/courses/Masterat/Xilinx%20Synthesis%20Technology/toolbox.xilinx.com/docsan/xilinx4/data/docs/xst/hdlcode8.html
+//shift register for the KEYBOARD decoder
+module s2p_shift( 
+ input logic clk; 
+ input logic DIN; 
+ input logic reset;
+ output logic [7:0] DO;
+); 
 
-module s2p_shift(C, SI, PO); 
-input  C,SI; 
-output [7:0] PO; 
-reg [7:0] tmp; 
+input  clk; 
+input logic tmp; 
  
-  always @(posedge C) 
+always_ff @(posedge clk, posedge reset) 
+  
+  
   begin 
     tmp = {tmp[6:0], SI}; 
   end 
