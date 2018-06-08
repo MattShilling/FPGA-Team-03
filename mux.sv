7d0c22d87503c33e060320886b4092299a524f10
steinbez <32185492+steinbez@users.noreply.github.com>
2018-06-07 21:09:42 -0700

    Update mux.sv

--- mux.sv
+++ mux.sv
@@ -4,7 +4,7 @@
 //Description: Allows user to switch between what input is desired for SNES control
 ///////////////////////////////////////////////////////
 module multiplexer(
-	input [7:0] key_mux,
+	input [7:0] key_mux, //assigning inputs and outputs
 	input [7:0] ir_mux,
 	input [7:0] b_mux,
 	input [1:0] dip,
@@ -13,7 +13,7 @@ module multiplexer(
 	
 always_comb	
 	begin
-		case(dip)
+		case(dip) //using the DIP switch on the FPGA to determine which input is sent to the output by a case statement
 			0:		mux_en = key_mux;
 			1:		mux_en = ir_mux;
 			2:		mux_en = b_mux;
