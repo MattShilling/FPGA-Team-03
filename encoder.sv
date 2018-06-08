dac0f1259e1db06f9023ad1a28bbdd2b38ebebf3
steinbez <32185492+steinbez@users.noreply.github.com>
2018-06-07 21:08:27 -0700

    Update encoder.sv

--- encoder.sv
+++ encoder.sv
@@ -4,14 +4,14 @@
 //Description: Converts signal from inputs into signal that the SNES can interpret
 ///////////////////////////////////////////////////////
 module snes_encoder (
-	input logic clock,
+	input logic clock, //assigning all inputs and outputs
 	input logic reset,
 	input logic load,
 	input logic [7:0] d,
 	output logic snes_output
 	);
 
-logic[7:0] q;
+	logic[7:0] q; //HDL equivalent of a wire, used as variable to set output to
 
 always_ff(posedge clk, posedge reset)
 	if (reset)
@@ -21,6 +21,6 @@ always_ff(posedge clk, posedge reset)
 	else
 		q <= {1, q[7:1]};
 		
-assign snes_out = q[0];
+	assign snes_out = q[0]; //sets the output to the first bit of the q variable
 
 endmodule
