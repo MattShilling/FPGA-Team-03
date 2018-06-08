ee4446eacd54ee92eb7e162c0ff642d4c5b68fbd
Marissa Kwon <kwonma@oregonstate.edu>
2018-06-07 18:15:49 -0700

    Update s2p_shift_register.sv

--- s2p_shift_register.sv
+++ s2p_shift_register.sv
@@ -1,6 +1,6 @@
 // http://www.csit-sun.pub.ro/courses/Masterat/Xilinx%20Synthesis%20Technology/toolbox.xilinx.com/docsan/xilinx4/data/docs/xst/hdlcode8.html
 
-module shift (C, SI, PO); 
+module s2p_shift(C, SI, PO); 
 input  C,SI; 
 output [7:0] PO; 
 reg [7:0] tmp; 
