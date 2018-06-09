3e7d63d0b43ebf28b5ced4fc50b4f8c5598b8546
MattShilling <shillinm@oregonstate.edu>
2018-06-08 22:03:41 -0700

    Update parity.v

--- KB_READER/parity.v
+++ KB_READER/parity.v
@@ -4,11 +4,13 @@
 //Description: module to check parity for the kb data
 ///////////////////////////////////////////////////////
  
- module parity(input [8:0] pdata, input en, input reset, output pass);
+ module parity(input [10:0] pdata, output wire pass);
 
-	reg [3:0] parity_count;
+	wire [3:0] parity_sum;
 	
-	always @(posedge en) begin
+	wire [1:0] start_stop;
+	
+	/*always @(posedge en) begin
 		if(pdata[0] == 1) parity_count <= parity_count + 1;
 		if(pdata[1] == 1) parity_count <= parity_count + 1;
 		if(pdata[2] == 1) parity_count <= parity_count + 1;
@@ -19,9 +21,25 @@
 		if(pdata[7] == 1) parity_count <= parity_count + 1;
 		if(pdata[8] == 1) parity_count <= parity_count + 1;
 		if (reset) parity_count <= 0;
-	end 
-		
-	assign pass = (parity_count % 2);
+	end*/
+	
+	assign parity_sum = pdata[1] + pdata[2] + pdata[3] + pdata[4] + pdata[5]
+				      + pdata[6] + pdata[7] + pdata[8] + pdata[9];
+	
+	assign start_stop = pdata[0] + pdata[10];
+	
+	// data line [1:8] and parity bit [9] always odd number of bits
+	// additionally, the start bit should always be 0 and stop, 1
+	assign pass = (parity_sum % 2) && (start_stop % 2);
+	
+	/*
+	always @(*) begin
+		parity_sum <= pdata[1] + pdata[2] + pdata[3] + pdata[4] + pdata[5]
+				      + pdata[6] + pdata[7] + pdata[8] + pdata[9];
+		start_stop <= pdata[0] + pdata[10];
+		pass <= (parity_sum % 2) && (start_stop % 2);
+	end
+	*/
 
 endmodule
 		
