3626c7d5ec0a1e6abc60be9ef2b940d57184dfaa
steinbez <32185492+steinbez@users.noreply.github.com>
2018-06-08 17:09:43 -0700

    Add files via upload

---
+++ cd10khz.sv
@@ -0,0 +1,21 @@
+////////////////////////////////////////////////////////
+//Author: Zach Steinberg
+//Module Name: Clock Divider
+//Description: Outputs a 10 kHz clock signal
+///////////////////////////////////////////////////////
+module clock_divider(
+	input logic clk,
+	input logic reset,
+	output logic clk_10khz
+	);
+	
+reg [7:0] counter;
+
+always_ff @ (posedge clk, negedge reset)
+	if counter > 200
+		clk = ~clk 
+		counter = 0
+	else
+		counter++
+
+endmodule
