ab6e7f09eeba727f0b7786ae01b4499895dd93c8
Marissa Kwon <kwonma@oregonstate.edu>
2018-06-08 20:00:44 -0700

    Update IR_decoder.sv

--- IR_decoder.sv
+++ IR_decoder.sv
@@ -5,20 +5,13 @@
 ///////////////////////////////////////////////////////
 module IR_decoder(
   input logic [31:0] IR_in,
-  output logic [31:0] IR_out,
+  output logic [7:0] IR_out,
   input logic latch
 );
 
   always_ff @ (posedge latch) 
   case(IR_in)
-    32'h00000A0B: IR_out <= 32'h0000_0001;  // B
-    32'h00000A02: IR_out <= 32'h0000_0002;  // Y
-    32'h00000A04: IR_out <= 32'h0000_0005;  // UP
-    32'h00000A06: IR_out <= 32'h0000_0006;  // DOWN
-    32'h00000A08: IR_out <= 32'h0000_0007;  // LEFT
-    32'h00000A10: IR_out <= 32'h0000_0008;  // RIGHT
-    32'h00000A0A: IR_out <= 32'h0000_0009;  // A
-    32'h00000A12: IR_out <= 32'h0000_000A;  // X
-    default: IR_out <= 32'h0000_0000;  // no change
+    32'hB857E02F: IR_out <= 8'b0000_1001;  // A
+    default: IR_out <= 8'b00000000;  // no change
   endcase
 endmodule
